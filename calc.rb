32a2ffa41a4c670e9ff45d0cd8d3da0d7ca67750
mghadley <matthew.g.hadley@gmail.com>
2016-05-11 14:32:55 -0600

    built calc

--- calc.rb
+++ calc.rb
@@ -1,18 +1,74 @@
-puts "Welcome to the Ruby Calculator"
-puts "Enter your first number"
+class String
+    def is_i?
+       !!(self =~ /\A[-+]?[0-9]+\z/)
+    end
 
-def sum (num1, num2)
-	num1 + num2
+    def is_f?
+	    !!Float(self) rescue false
+	  end
+
+    def is_operator?(str)
+    	operators = ['+','/','*','**','%','-']
+    	operators.include? str
+    end
 end
 
-def divide (num1, num2)
-	num1 / num2
+def confirm_float(str)
+	unless str.is_f?
+		puts "*******INCORRECT INPUT TYPE********"
+		main
+	end
 end
 
-def is_prime?(n)
-	divisors = 0
-	return false if n <= 1
-	return true if n ==2
-	(2..(n/2)).each do |x|
-		if n %
+def confirm_operator(str)
+	unless str.is_operator? str 
+		puts "\n*******INCORRECT INPUT TYPE********", "\n"
+		main
+	end
 end
+
+def divide_by_zero?(operator, num2)
+	if operator == "/" && num2 == 0
+		puts "\n********NO DIVIDING BY 0********", "\n"
+		main
+	end
+end
+
+def get_input
+	user_input = gets.strip.downcase
+	if user_input == 'clear'
+		main
+	elsif user_input == 'quit'
+		exit
+	else
+		return user_input
+	end
+end
+		
+
+def main
+	while true
+		puts "\nWelcome to the Ruby Calculator"
+		puts "Type 'quit' at anytime to quit"
+		puts "Type 'clear' at anytime to start over"
+		puts "\nEnter your first number"
+		print ">"
+		num1 = get_input
+		confirm_float num1
+		num1 = num1.to_f
+		puts "Enter your operator"
+		print ">"
+		operator = get_input
+		confirm_operator operator
+		puts "Enter your second number"
+		print ">"
+		num2 = get_input
+		confirm_float num2
+		num2 = num2.to_f
+		divide_by_zero?(operator,num2)
+		puts "\nANSWER:", num1.method(operator).(num2), "\n"
+	end
+end
+
+main
+
