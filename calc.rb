b5bc16053a86aaa64b39a9129d0c041e75eb178b
mghadley <matthew.g.hadley@gmail.com>
2016-05-12 15:41:03 -0600

    cleaned up code

--- calc.rb
+++ calc.rb
@@ -20,22 +20,6 @@ def confirm_float(str)
 	end
 end
 
-#NOT USING ANYMORE
-# def confirm_operator(str)
-# 	unless str.is_operator? str 
-# 		puts "\n*******INCORRECT INPUT TYPE********", "\n"
-# 		main
-# 	end
-# end
-
-#NOT USING ANYMORE
-# def divide_by_zero?(operator, num2)
-# 	if operator == "/" && num2 == 0
-# 		puts "\n********NO DIVIDING BY 0********", "\n"
-# 		main
-# 	end
-# end
-
 def get_input
 	user_input = gets.strip.downcase
 	if user_input == 'clear' 
@@ -59,11 +43,6 @@ def get_input
 	end
 end
 
-#NOT USING ANYMORE
-# def to_array(phrase)
-# 	phrase.gsub(/[(]/, "|(").gsub(/[)]/, ")|").split("|").reject{ |i| i.empty? }
-# end
-
 def get_trig_input
 	puts "Enter the number you would like to calcuate this for"
 	user_input = get_input.gsub(/m/, "#{@memory}")
@@ -79,30 +58,6 @@ def show_history
 	end
 	main
 end
-
-#OLD FUNCTION FOR NON-BONUS PART
-# def main
-# 	while true
-# 		puts "Type 'quit' at anytime to quit"
-# 		puts "Type 'clear' at anytime to start over"
-# 		puts "\nEnter your first number"
-# 		print ">"
-# 		num1 = get_input
-# 		confirm_float num1
-# 		num1 = num1.to_f
-# 		puts "Enter your operator"
-# 		print ">"
-# 		operator = get_input
-# 		confirm_operator operator
-# 		puts "Enter your second number"
-# 		print ">"
-# 		num2 = get_input
-# 		confirm_float num2
-# 		num2 = num2.to_f
-# 		divide_by_zero?(operator,num2)
-# 		puts "\nANSWER:", num1.method(operator).(num2), "\n"
-# 	end
-# end
 
 def main
 	while true
